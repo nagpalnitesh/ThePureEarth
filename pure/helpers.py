9004c6b3dce68c9e6f1457a7f742972b3cd5f17a
Nitesh Nagpal <niteshnagpal@outlook.com>
2021-11-11 19:40:27 +0530

    pure earth

--- pure/helpers.py
+++ pure/helpers.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 def send_forget_password_token(email, token):
     subject = 'Your foget password link'
-    message = f'Hi, verify your account: http://127.0.0.1:8000/reset-password/{token}'
+    message = f'Hi, verify your account: https://pureearth.herokuapp.com/reset-password/{token}'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, email_from, recipient_list)
--- pure/views.py
+++ pure/views.py
@@ -145,7 +145,7 @@ def handleSignup(request):
 # activate account function
 def activate_account(email, token):
     subject = 'Activate your account'
-    message = f'Hi, verify your account: http://127.0.0.1:8000/verify/{token}'
+    message = f'Hi, verify your account: https://pureearth.herokuapp.com/verify/{token}'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, email_from, recipient_list)
