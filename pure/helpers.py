aa653d360b6f0266ca60da3b382c8af3e176e1ee
nagpalnitesh <niteshnagpal@outlook.com>
2023-02-05 01:25:57 +0530

    mail content update

--- pure/helpers.py
+++ pure/helpers.py
@@ -4,7 +4,24 @@ from django.conf import settings
 
 def send_forget_password_token(email, token):
     subject = 'Your foget password link'
-    message = f'Hi, verify your account: https://pureearth.herokuapp.com/reset-password/{token}'
+    message = f'''
+    Hi,
+
+We hope this email finds you well. We're writing to let you know that you recently signed up for an account on Pure Earth, the online platform that connects individuals and organizations with sustainability initiatives around the world.
+
+To complete your profile and start making a positive impact, we need to verify your email address. This is a quick and easy step that helps us ensure that you are who you say you are.
+
+Please click on the link below to verify your Pure Earth profile:
+
+https://pureearth.herokuapp.com/reset-password/{token}
+
+If you did not sign up for an account on Pure Earth, please ignore this email and your account will not be created.
+
+We're excited to have you on board and can't wait to see the positive impact you will make in the world!
+
+Best regards,
+
+The Pure Earth Team'''
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, email_from, recipient_list)
--- pure/views.py
+++ pure/views.py
@@ -179,7 +179,23 @@ def handleSignup(request):
 # activate account function
 def activate_account(email, token):
     subject = 'Activate your account'
-    message = f'Hi, verify your account: https://thepureearth.com/verify/{token}'
+    message = f'''Hi,
+
+We hope this email finds you well. We're writing to let you know that you recently signed up for an account on Pure Earth, the online platform that connects individuals and organizations with sustainability initiatives around the world.
+
+To complete your profile and start making a positive impact, we need to verify your email address. This is a quick and easy step that helps us ensure that you are who you say you are.
+
+Please click on the link below to verify your Pure Earth profile:
+
+https://pureearth.herokuapp.com/reset-password/{token}
+
+If you did not sign up for an account on Pure Earth, please ignore this email and your account will not be created.
+
+We're excited to have you on board and can't wait to see the positive impact you will make in the world!
+
+Best regards,
+
+The Pure Earth Team'''
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, email_from, recipient_list)
