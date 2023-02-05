5c07ce1fd613668cd537d839d0f03f6c82b85863
nagpalnitesh <niteshnagpal@outlook.com>
2023-02-05 13:05:37 +0530

    mail content update

--- pure/views.py
+++ pure/views.py
@@ -179,7 +179,23 @@ def handleSignup(request):
 # activate account function
 def activate_account(email, token):
     subject = 'Activate your account'
-    message = f'Hi, verify your account: https://thepureearth.com/verify/{token}'
+    message = f'''Hi,
+
+We hope this email finds you well. We're writing to let you know that you recently signed up for an account on Pure Earth, the online platform that connects individuals and organizations with sustainability initiatives around the world.
+
+To complete your profile and start making a positive impact, we need to verify your email address. This is a quick and easy step that helps us ensure that you are who you say you are.
+
+Please click on the link below to verify your Pure Earth profile:
+
+https://thepureearth.com/verify/{token}
+
+If you did not sign up for an account on Pure Earth, please ignore this email and your account will not be created.
+
+We're excited to have you on board and can't wait to see the positive impact you will make in the world!
+
+Best regards,
+
+The Pure Earth Team'''
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, email_from, recipient_list)
@@ -248,9 +264,11 @@ def change_password(request, token):
             if newpass != newconfpass:
                 messages.error(request, 'Password does not match')
                 return redirect('/reset-password/{token}')
+            print(newpass, newconfpass)
             user_obj = User.objects.get(id=user_id)
             user_obj.set_password(newpass)
             user_obj.save()
+            print(user_obj)
             messages.success(request, 'Password changed successfully')
             return redirect('/login')
 
